a565c78628ac59044fd779860adddc9f491b41f5
DESKTOP-ISRPCIK\Kria <krianoto@gmail.com>
2018-03-19 06:55:16 +1030

    no message

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public abstract class Controller : MonoBehaviour
+{
+    protected Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    protected abstract void applyMovement();
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+
+public class PlayerController : Controller
+{
+    public float moveSpeed = 1f;
+    public float jumpSpeed = 6f;
+    private Vector3 movement;
+
+	void Start ()
+    {
+	}
+
+    void Update()
+    {
+        applyMovement();
+    }
+
+    //TODO: Add condition to check we are grounded, add it to jump condition
+    protected override void applyMovement()
+    {
+        float jump = 0;
+
+        movement = Vector3.zero;
+
+        movement += Vector3.forward * Input.GetAxis("Vertical") * moveSpeed;
+        movement += Vector3.right * Input.GetAxis("Horizontal") * moveSpeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+            jump = jumpSpeed;
+
+        rb.velocity = new Vector3(movement.x, rb.velocity.y + jump, movement.z);
+    }
+}
