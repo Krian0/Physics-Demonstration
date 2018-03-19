e1d2de8a7705cf531f282516e56083d184ab5d08
AIE\s170958 <krianoto@gmail.com>
2018-03-19 17:09:31 +1030

    TODO: Fix crouching, do ceiling test.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,34 +1,105 @@
 ﻿using System;
 using UnityEngine;
 
-
-public class PlayerController : Controller
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 1f;
-    public float jumpSpeed = 6f;
+    public Camera FPSCam;
+    public float mouseSensitivity = 4;
+    public float camRotationMaxClamp = 40;
+    public float camRotationMinClamp = -30;
+
+    public float walkSpeed = 10f;
+    public float runSpeed = 14f;
+    public float jumpSpeed = 4f;
+    public float crouchSpeed = 2f;
+    public float gravity = 9.81f;
+
+    public float groundDetectionBuffer = 0.1f;
+    public float ceilingDetectionBuffer = 0.2f;
+
+    public bool isGrounded;
+    public bool canStand;
+    public bool doJump;
+    public bool doRun;
+    public bool doCrouch;
+
+    private float rotPitch;
     private Vector3 movement;
+    private CharacterController cc;
 
 	void Start ()
     {
+
 	}
+
+    void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
-        applyMovement();
+        detectActions();
+        resolveActions();
+        rotatePlayer();
+        movePlayer();
     }
 
-    //TODO: Add condition to check we are grounded, add it to jump condition
-    protected override void applyMovement()
+    void detectActions()
     {
-        float jump = 0;
+        isGrounded = Physics.Raycast(transform.position + 0.1f * Vector3.up, Vector3.down, groundDetectionBuffer);
+        //canStand = !Physics.Raycast(transform.position + new Vector3(0, cc.height, 0) - 0.1f * Vector3.up, Vector3.down, ceilingDetectionBuffer);
+        doJump = Input.GetKey(KeyCode.Space);
+        doRun = (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift));
+        doCrouch = (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl));
+    }
 
-        movement = Vector3.zero;
+    void resolveActions()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            cc.height /= 8;
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+            cc.height *= 8;
+    }
 
-        movement += Vector3.forward * Input.GetAxis("Vertical") * moveSpeed;
-        movement += Vector3.right * Input.GetAxis("Horizontal") * moveSpeed;
-        if (Input.GetKeyDown(KeyCode.Space))
-            jump = jumpSpeed;
+    void rotatePlayer()
+    {
+        float rotYaw = Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotPitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        FPSCam.transform.localRotation = Quaternion.Euler(Mathf.Clamp(rotPitch, camRotationMinClamp, camRotationMaxClamp), 0, 0);
+        transform.Rotate(0, rotYaw, 0);
+    }
 
-        rb.velocity = new Vector3(movement.x, rb.velocity.y + jump, movement.z);
+    void movePlayer()
+    {
+        float previousY = movement.y;
+        movement = transform.TransformDirection(getMovement()) * getMoveSpeed();
+
+        if (!isGrounded)
+            movement.y = previousY;
+
+        movement.y += getYInput() - gravity * Time.deltaTime;
+        cc.Move(movement * Time.deltaTime);
+    }
+
+    Vector3 getMovement()
+    {
+        return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    float getYInput()
+    {
+        return (doJump && isGrounded) ? jumpSpeed : 0;
+    }
+
+    float getMoveSpeed()
+    {
+        if (doRun && isGrounded)
+            return runSpeed;
+        if (doCrouch && isGrounded)
+            return crouchSpeed;
+
+        return walkSpeed;
     }
 }
